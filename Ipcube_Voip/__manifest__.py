81c898cb37e98dcde547d946f5fe5b3fcff4b051
rafaelfmartinez <90006734+rafaelfmartinez@users.noreply.github.com>
2022-10-26 00:57:03 -0400

    Update __manifest__.py

--- Ipcube_Voip/__manifest__.py
+++ Ipcube_Voip/__manifest__.py
@@ -8,7 +8,7 @@
 
     'author': "IPCUBE",
     'website': "https://ipcube.net",
-
+    'license': "LGPL-3",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
