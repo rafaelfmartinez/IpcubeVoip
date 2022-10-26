f185ea53137dfaf02ffbd99ff9676293347dfa6f
rafaelfmartinez <90006734+rafaelfmartinez@users.noreply.github.com>
2022-10-25 23:39:33 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -9,13 +9,13 @@
        "Ipcube provides voip connectivity to businesses. Our IPCUBE app simplifies connection of your Odoo implementation to the local and international telephony network. You may use Ipcube connector to integrate your existing voice provider or using the Ipcube network.  Now your Odoo users will be able to make and receive phone calls in their browsers from other users or from any phone and to any phone."
     """,
 
-    'author': "IP CUBE",
-    'website': "",
+    'author': "IPCUBE",
+    'website': "https://ipcube.net",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Applications',
+    'category': 'Productivity',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
