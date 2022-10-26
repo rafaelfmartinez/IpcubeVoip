d92aece3db66c53d0eeb4a79a7fdc9b5b89eb661
rafaelfmartinez <90006734+rafaelfmartinez@users.noreply.github.com>
2022-10-25 22:50:58 -0400

    Update res_config_settings.py

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -5,9 +5,9 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     wsServer = fields.Char("WebSocket", help="The URL of your WebSocket",
-                           default='wss://voip.ipcube.net:8069/ws', config_parameter='voip.wsServer')
+                           default='wss://voip.ipcube.net:8069/ws', config_parameter='voip.wsServer', {'readonly': True})
     pbx_ip = fields.Char("PBX Server IP", help="The IP adress of your PBX Server",
-                         default='voip.ipcube.net', config_parameter='voip.pbx_ip')
+                         default='voip.ipcube.net', config_parameter='voip.pbx_ip', {'readonly': True})
     mode = fields.Selection([
         ('demo', 'Demo'),
         ('prod', 'Production'),
