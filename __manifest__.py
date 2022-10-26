50863c9b7bc81eb62e74a55cd33503013ba0f6a6
rafaelfmartinez <90006734+rafaelfmartinez@users.noreply.github.com>
2022-10-26 00:24:26 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Integration of VOIP with IPCUBE",
+    'name': "IPCUBE_VOIP",
 
-    'summary': """
-        Integration of VOIP with IPCUBE""",
+    'summary': "Integration of VOIP with IPCUBE",
 
-    'description': """
-       "Ipcube provides voip connectivity to businesses. Our IPCUBE app simplifies connection of your Odoo implementation to the local and international telephony network. You may use Ipcube connector to integrate your existing voice provider or using the Ipcube network.  Now your Odoo users will be able to make and receive phone calls in their browsers from other users or from any phone and to any phone."
-    """,
+    'description': "Ipcube provides voip connectivity to businesses. Our IPCUBE app simplifies connection of your Odoo implementation to the local and international telephony network. You may use Ipcube connector to integrate your existing voice provider or using the Ipcube network.  Now your Odoo users will be able to make and receive phone calls in their browsers from other users or from any phone and to any phone.",
 
     'author': "IPCUBE",
     'website': "https://ipcube.net",
