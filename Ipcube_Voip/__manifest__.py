d616e0b24e127e4864fe2c37f581fd99583d5283
rafaelfmartinez <90006734+rafaelfmartinez@users.noreply.github.com>
2022-10-30 10:59:39 -0400

    Update __manifest__.py

--- Ipcube_Voip/__manifest__.py
+++ Ipcube_Voip/__manifest__.py
@@ -23,6 +23,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/res_config_settings_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
